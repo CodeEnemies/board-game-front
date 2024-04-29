0d00bfa5f7872130b00523cb0c19490132600f5a
Gadelha TI <marcelo.gadelha@mairnha.mil.br>
2024-04-29 14:48:33 -0300

    chore: add status

--- src/components/character.tsx
+++ src/components/character.tsx
@@ -1,4 +1,6 @@
 export interface Character {
+    // status aumenta durante o modo sanin
+    status: number;
     hp: number;
     mana: number;
     stamina?: number;
@@ -6,6 +8,7 @@ export interface Character {
 }
 
 export const initialCharacter: Character = {
+    status: 0,
     hp: 0,
     mana: 0,
     stamina: 0,
